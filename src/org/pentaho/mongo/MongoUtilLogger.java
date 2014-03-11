5fc2a5ab20aa91003291b2bcb13f2bcecf28645c
Kurtis Walker <kwalker@pentaho.com>
2014-03-11 14:05:32 -0400

    [ANALYZER-2449] - adding methods to delegate count and distinct.  Also, cleaning up the logging interface.

--- src/org/pentaho/mongo/MongoUtilLogger.java
+++ src/org/pentaho/mongo/MongoUtilLogger.java
@@ -5,7 +5,9 @@ public interface MongoUtilLogger {
 
   void debug( java.lang.String s );
 
-  void info( java.lang.String s, java.lang.Object... objects );
+  void info( java.lang.String s );
+
+  void warn( java.lang.String s, java.lang.Throwable throwable );
 
   void error( java.lang.String s, java.lang.Throwable throwable );
 
