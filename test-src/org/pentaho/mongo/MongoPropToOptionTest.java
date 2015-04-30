2a913cd3e0b200696102823913bac5cdd7ab0eec
Kanstantsin Karneliuk <kanstantsin_karneliuk@epam.com>
2015-04-30 21:17:13 +0300

    [TEST] fixing tests after driver switch
    What was done:
    1) fixed a unit test issue after driver upgrade

--- test-src/org/pentaho/mongo/MongoPropToOptionTest.java
+++ test-src/org/pentaho/mongo/MongoPropToOptionTest.java
@@ -23,10 +23,10 @@ import com.mongodb.util.JSON;
 import com.mongodb.util.JSONParseException;
 import org.junit.Test;
 
-import static com.mongodb.util.MyAsserts.assertFalse;
 import static junit.framework.Assert.fail;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
